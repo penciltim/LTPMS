9d629e3e8d7aa7a9b645acfe9eabe237870f5191
Tim <penciltim@gmail.com>
2013-03-07 01:10:54 +0800

    fixed"can only upload file to outof source code folder"

--- ltpms/settings.py
+++ ltpms/settings.py
@@ -47,7 +47,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'uploads/')
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
